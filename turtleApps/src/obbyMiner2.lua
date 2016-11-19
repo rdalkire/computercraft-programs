ebca3d45c2b859be7655b3e66566284c1fd82470
rdalkire <dave.alkire@gmail.com>
2016-11-19 15:26:45 -0500

    trying to get started but at different computer

---
+++ turtleApps/src/obbyMiner2.lua
@@ -0,0 +1,19 @@
+--[[ Obsidian Miner 2
+
+Copyright (c) 2016
+Robert David Alkire II, IGN ian_xw
+Distributed under the MIT License.
+(See accompanying file LICENSE or copy
+at http://opensource.org/licenses/MIT)
+]]
+
+local function main( args )
+  
+  -- From args, learn: do I get it all
+  -- or just one layer?
+  
+  
+  
+end
+
+main({...})
