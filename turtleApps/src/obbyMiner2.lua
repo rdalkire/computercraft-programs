751a76e2871d8f0fcaa097a2f199d612bcfac9cb
rdalkire <dave.alkire@gmail.com>
2016-11-24 10:32:29 -0500

    slightly started

--- turtleApps/src/obbyMiner2.lua
+++ turtleApps/src/obbyMiner2.lua
@@ -7,12 +7,49 @@ Distributed under the MIT License.
 at http://opensource.org/licenses/MIT)
 ]]
 
+local getopt= require "getopt"
+
+local onlyOneLayer = false
+
+local function initOptions( args )
+
+  local someOptions = {
+    ["one"] = { "(o)ne layer only", 
+        "o", nil}
+  }
+  
+  local tbl= getopt.init(
+      "Obsidian Miner 2",
+      "Mines obby from lava pit",
+      someOptions, args )
+      
+  if tbl ~= nil then
+    if tbl["one"] then
+      onlyOneLayer= true
+    end
+  end
+
+end
+
+local function mineALayer()
+  -- TODO design, implement mineALayer
+  return false--TODO rtrn 4 mineALayer 
+end
+
 local function main( args )
   
-  -- From args, learn: do I get it all
+  -- From args, learns: get it all
   -- or just one layer?
+  initOptions(args)
   
+  local keepGoing = true
   
+  while keepGoing do
+    keepGoing= mineALayer()
+    if onlyOneLayer then
+      keepGoing = false
+    end
+  end
   
 end
 
