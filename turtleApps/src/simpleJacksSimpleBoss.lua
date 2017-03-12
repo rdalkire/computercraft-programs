e0e4a5f2892f2c53360120b946d94c1ebee00efd
rdalkire <dave.alkire@gmail.com>
2017-03-12 16:10:45 -0400

    improved simpleJacksSimpleBoss (hopefully)

--- turtleApps/src/simpleJacksSimpleBoss.lua
+++ turtleApps/src/simpleJacksSimpleBoss.lua
@@ -2,9 +2,32 @@
 -- but with 4x4 thick trunks
 -- Start at the close left one
 
--- local t = require("mockTurtle")
+local D_BASE = "https://"..
+  "raw.githubusercontent.com/"..
+  "rdalkire/"..
+  "computercraft-programs/"..
+  "dalkire-obsidian2/turtleApps/src/"
+
+--- Ensures dependency exists.
+local function ensureFile(depNme)
+
+  print("Ensuring presence of "..
+      depNme )
+
+  local drFile= loadfile( depNme )
+
+  if drFile == nil then
+    
+    shell.run("wget",
+      D_BASE.. depNme, depNme )
+  end
+
+end
+
 local t = turtle
-local smpljck = "simplejack"
+local smpljck = "simpleJack.lua"
+
+ensureFile(smpljck)
 
 for i= 1, 2 do
  shell.run(smpljck)
