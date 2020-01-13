6674ec0a7f77f48c597d8b5de81ad0e3b023e5b1
rdalkire <dave.alkire@gmail.com>
2020-01-12 21:57:23 -0500

    External call to the veinMiner script

--- turtleApps/src/exploreAndMine.lua
+++ turtleApps/src/exploreAndMine.lua
@@ -4,21 +4,44 @@
 -- vicinity.  So you place your turtle
 -- facing a blank wall, fuel it up, put
 --  a chest behind it, and launch 
--- "exploreAndMine". The turtle looks 
+-- "exploreAndMine". 
+
+-- The turtle looks 
 -- fore, up, down, left and right, but 
+
 -- (assuming this really is a blank 
--- wall) then digs ahead, moves 
--- forward and repeats until it finds 
+-- wall)
+ 
+-- then digs ahead, moves 
+-- forward and 
+
+-- repeats until it finds 
 -- something interesting (iron, 
 -- diamond, coal, redstone or 
--- something).  Along the way it makes 
--- a 2-block high tunnel and every 10 
+-- something).  
+
+-- Along the way it makes 
+-- a 2-block high tunnel and 
+
+-- every 10 
 -- blocks it places a torch to its 
--- right. When it finally finds 
--- something it runs Veinminer and 
+-- right. 
+
+-- When it finally finds 
+-- something 
+
+-- it runs Veinminer and 
 -- creates a nice rectangular room, 
--- leaves a torch on the floor then 
+local SCRIPT = {"veinMiner", "-r"}
+shell.run( unpack(SCRIPT) )
+
+-- leaves a torch on the floor 
+
+--then 
 -- continues forward from there.
+
 -- It continues like this limited by a 
 -- maximum block distance from its 
 -- starting place, or limited by fuel.
+
+
